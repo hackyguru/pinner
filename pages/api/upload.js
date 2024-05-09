da49d852adea4dd6ed3e669165295ab3e35d2e6e
Kumaraguru <19eucs071@skcet.ac.in>
2024-05-09 22:35:41 +0530

    initial commit

---
+++ pages/api/upload.js
@@ -0,0 +1,67 @@
+import lighthouse from '@lighthouse-web3/sdk';
+import formidable from 'formidable';
+import fs from 'fs';
+import path from 'path';
+import Cors from 'cors';
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ['POST'],
+});
+
+// Helper method to wait for NodeJS to close the incoming stream
+function parseForm(req) {
+  return new Promise((resolve, reject) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ fields, files });
+      }
+    });
+  });
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  
+  try {
+      // Run the cors middleware
+    await cors(req, res);
+  
+    const apiKey = process.env.LIGHTHOUSE_API_KEY;
+  
+    if (req.method === 'POST') {
+      try {
+        const { files } = await parseForm(req);
+        const uploadedFiles = files.files.map((file) => file.filepath);
+        const uploadResponse = await lighthouse.upload(uploadedFiles, apiKey, {
+          uploadDirectory: true,
+        });
+        const cid = uploadResponse.data.Hash;
+  
+        return res.status(200).json({ cid });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error uploading to IPFS' });
+      } finally {
+        // Clean up temporary files
+        files.files.forEach((file) => {
+          fs.unlinkSync(file.filepath);
+        });
+      }
+    } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+
+  } catch(e) {
+    console.log("console error :",e)
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+
+const HomePage = () => {
+
+  return <div>GitVault API</div>;
+};
+
+export default HomePage;
